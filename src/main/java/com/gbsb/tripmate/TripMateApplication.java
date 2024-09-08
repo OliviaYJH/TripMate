db3996e8bbc2b33ee83d0972a0e12c51ec728b18
YuJeongHyun <0703olivia@naver.com>
2024-09-08 22:51:37 +0900

    refactor: Edit group name

--- src/main/java/com/gbsb/tripmate/TripMateApplication.java
+++ src/main/java/com/gbsb/tripmate/TripMateApplication.java
@@ -1,4 +1,4 @@
-package com.example.tripmate;
+package com.gbsb.tripmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gbsb/tripmate/TripMateApplicationTests.java
+++ src/test/java/com/gbsb/tripmate/TripMateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tripmate;
+package com.gbsb.tripmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
