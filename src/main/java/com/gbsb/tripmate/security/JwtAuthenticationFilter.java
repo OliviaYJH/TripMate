8a806257ea1d77193e71de9ecc5fb5b90d38f34e
JHKim9824 <jeonghunkim98@gmail.com>
2024-09-18 23:29:49 +0900

    refactor: PR #7 troubleshooting

--- src/main/java/com/gbsb/tripmate/security/JwtAuthenticationFilter.java
+++ src/main/java/com/gbsb/tripmate/security/JwtAuthenticationFilter.java
@@ -40,7 +40,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 String username = jwtUtil.getUsernameFromJWT(jwt);
 
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
