fa34e0217206e1d0831f54c3406d94a1cfa73c3e
Baekchoi <magnituzii@naver.com>
2024-09-29 20:14:24 +0900

    feat: Add real time chat

---
+++ src/main/java/com/gbsb/tripmate/service/ChatService.java
@@ -0,0 +1,60 @@
+package com.gbsb.tripmate.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gbsb.tripmate.config.WebSocketConfig;
+import com.gbsb.tripmate.dto.ChatRoom;
+import com.gbsb.tripmate.entity.ChatMessage;
+import com.gbsb.tripmate.enums.ErrorCode;
+import com.gbsb.tripmate.exception.MeetingException;
+import com.gbsb.tripmate.repository.ChatMessageRepository;
+import com.gbsb.tripmate.repository.ChatRoomRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        ChatRoom chatRoom = ChatRoom.builder()
+                .roomId(randomId)
+                .name(name)
+                .build();
+        chatRooms.put(randomId, chatRoom);
+        return chatRoom;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
+
